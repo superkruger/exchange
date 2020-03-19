120ac0cb2d53bd5197217dcc49859ed9dc3c6291
Jaco Kruger <superkruger@gmail.com>
2020-03-19 11:02:31 +0100

    Updates

---
+++ precache-manifest.27b8a9ddaeab1ec3b3abbe4e3da8d601.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827b331484fd18311e1246261daaa8da",
+    "url": "/exchange/index.html"
+  },
+  {
+    "revision": "8e1ab8ea5ea19e1897a5",
+    "url": "/exchange/static/js/3.8a3dfa53.chunk.js"
+  },
+  {
+    "revision": "aa2f507c9918131887a7d06ce165be13",
+    "url": "/exchange/static/js/3.8a3dfa53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806fb482ebf7593a9e8c",
+    "url": "/exchange/static/js/4.a4cf42fe.chunk.js"
+  },
+  {
+    "revision": "60d951e6607d4eabeaed",
+    "url": "/exchange/static/js/5.0eebf803.chunk.js"
+  },
+  {
+    "revision": "b1711d32312ed0df1b5e",
+    "url": "/exchange/static/js/6.b7dbf34f.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/exchange/static/js/6.b7dbf34f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07b10826e4252341d55f",
+    "url": "/exchange/static/js/main.91d7a8ff.chunk.js"
+  },
+  {
+    "revision": "2e5db9b0cfe96d3a3263",
+    "url": "/exchange/static/js/runtime-main.d532e339.js"
+  },
+  {
+    "revision": "b1cf4ae394ee14a089d5",
+    "url": "/exchange/static/js/xlsx.9b2e9682.chunk.js"
+  }
+]);
---
+++ static/js/xlsx.9b2e9682.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpexchange=this.webpackJsonpexchange||[]).push([[2],{561:function(n,c){},565:function(n,c){},566:function(n,c){}}]);
+//# sourceMappingURL=xlsx.9b2e9682.chunk.js.map
