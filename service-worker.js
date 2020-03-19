7359aa7d42f802ce0c50e4103a2d252fe684b5c1
Jaco Kruger <superkruger@gmail.com>
2020-03-19 15:01:22 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "http://superkruger.github.io/exchange/precache-manifest.fc8613839c7a988d3e9aa30bae061a58.js"
+  "https://superkruger.github.io/exchange/precache-manifest.35a201ead9db5be80e00083b10fc5cb3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://superkruger.github.io/exchange/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://superkruger.github.io/exchange/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
