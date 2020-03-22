085180a80fd9c4a4b6a95b526fa4fbafee36a78d
Jaco Kruger <superkruger@gmail.com>
2020-03-22 15:14:39 +0100

    Updates

---
+++ precache-manifest.3d765e0e056ab6ba8967bfcafc488bde.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6c5c476150006a54eff41d896c03ca",
+    "url": "/exchange/index.html"
+  },
+  {
+    "revision": "30f6c89500ee44714ca6",
+    "url": "/exchange/static/js/3.af5c75d5.chunk.js"
+  },
+  {
+    "revision": "e6ee003e68cc623109cdbdde487ce090",
+    "url": "/exchange/static/js/3.af5c75d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10161287f19046bf63a6",
+    "url": "/exchange/static/js/4.7b57ab78.chunk.js"
+  },
+  {
+    "revision": "c736b3622c31d17e9cea",
+    "url": "/exchange/static/js/5.58887eaa.chunk.js"
+  },
+  {
+    "revision": "eecfb1ca9c94e1adf463",
+    "url": "/exchange/static/js/6.500f82b8.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/exchange/static/js/6.500f82b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7116a60c8b41632d021",
+    "url": "/exchange/static/js/main.d57984cd.chunk.js"
+  },
+  {
+    "revision": "d8fa5ac05189c6003e5b",
+    "url": "/exchange/static/js/runtime-main.c419d384.js"
+  },
+  {
+    "revision": "26f15a43141f500192a8",
+    "url": "/exchange/static/js/xlsx.d1ace490.chunk.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3b53795735bf64a5e35870f4c48d7b14.js"
+  "/exchange/precache-manifest.3d765e0e056ab6ba8967bfcafc488bde.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/exchange/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
